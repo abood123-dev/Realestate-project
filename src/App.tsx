bb0c34dd7d5708fa5d9c1089882c0c621f8f7a0d
Abood <abood@gmail.com>
2025-04-05 14:03:51 -0700

    Building the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import { HashRouter as Router,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      hello
-      </header>
-    </div>
+    <Router>
+      <Routes>
+       <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
+    
   );
 }
 // Remove-Item -Recurse -Force docs
---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import '../App.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+function Home() {
+  const [Isvisible,setIsvisible]=useState(true);
+  const handleScroll=()=>
+    {
+      const ScrollY=window.scrollY;
+      if(ScrollY ===0 || ScrollY > 90)
+        {
+          setIsvisible(true);
+        }
+        else
+        {
+          setIsvisible(false);
+        }
+    }
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [Isvisible]);
+const Header :React.CSSProperties = 
+{
+paddingTop: '20',
+backgroundColor: '#f5f5f5',
+top: '0',
+paddingBottom:"20",
+opacity: Isvisible ? 1 : 0,
+transition: "ease-out 0.3s",
+position:"fixed",
+zIndex:'100',
+width:"100%"
+}
+useEffect(()=>
+  {
+    window.scrollTo(0,0)
+  },[])
+  return (
+    <>
+      <header>
+      <div className="Header" style={Header}>
+     <Link to="/" style={{textDecoration:'none'}}><div className="villa">Villa</div> </Link>  
+      <div className="secondop">
+      <Link to='/' style={{textDecoration:'none',color: 'coral'}}>  <div>Home</div> </Link> <Link to='/Listing' style={{textDecoration:'none',color:'black' }}><div>Properties</div></Link> <Link to='/Visits' style={{textDecoration:'none',color:'black' }}>  <div>Your visits</div> </Link> <Link to='/Contact' style={{textDecoration:'none',color:'black' }}><div>Contact us</div></Link> <Link to='/Listing' style={{textDecoration:'none',color:"black"}}>  <div className="calender"><img src='https://th.bing.com/th/id/OIP.04r7fFOqv3ONvvJZfokfMwAAAA?rs=1&pid=ImgDetMain' alt="" width="25" height="25" className="whitecal"  /> </div> <div className="your">Schedule a visit</div> </Link>  
+      </div>
+      </div>  
+      </header>
+      </>
+  );
+}
+
+export default Home;
