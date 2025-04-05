36159ff7de2f6bf135eb0df7df3c19fde6347c6d
Abood <abood@gmail.com>
2025-04-05 13:19:28 -0700

    Beginning my project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      hello
+      </header>
+    </div>
+  );
+}
+// Remove-Item -Recurse -Force docs
+export default App;
